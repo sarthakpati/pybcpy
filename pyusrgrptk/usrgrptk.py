97776192caf01461f2103968081e68abaf940557
kr-g <k.r.goger@gmail.com>
2020-11-12 09:50:49 +0100

    added user group repo, load and write methods

--- pyusrgrptk/usrgrptk.py
+++ pyusrgrptk/usrgrptk.py
@@ -17,7 +17,8 @@ class Group(object):
     def loads(self, grp_info):
         group = grp_info.strip().split("\t")
         self.name = group[0]
-        self.member = list(map(lambda x: x.strip(), group[1].split(",")))
+        if len(group) > 1:
+            self.member = list(map(lambda x: x.strip(), group[1].split(",")))
         return self
 
     def dumps(self):
@@ -36,25 +37,68 @@ class Group(object):
         )
 
 
-all_grps = []
-glist = []
-gmap = {}
-gmap_keys = []
+class GroupRepo(object):
+    def __init__(self, store_file=None):
+        self.store_file = store_file
+        if self.store_file == None:
+            self.store_file = os.path.join("~", ".grpinfo")
+        self.store_file = os.path.expanduser(self.store_file)
+        self.groups = []
+        self.gmap = {}
+        self.gmap_keys = []
 
+    def __repr__(self):
+        return (
+            self.__class__.__name__
+            + "("
+            + " store: '"
+            + self.store_file
+            + "'"
+            + " groups: "
+            + str(len(self.groups))
+            + " )"
+        )
 
-def get_all_groups():
-    global all_grps
-    all_grps = grp.getgrall()
+    def read_current(self):
+        all_grps = grp.getgrall()
+        self.groups = map(lambda x: Group().from_grp(x), all_grps)
+        self._init_internal()
+        return self
 
-    global glist
-    glist = map(lambda x: Group().from_grp(x), all_grps)
-    glist = sorted(glist, key=lambda x: x.name)
+    def _init_internal(self):
+        self.groups = sorted(self.groups, key=lambda x: x.name)
+        self.gmap = dict(map(lambda x: (x.name, x), self.groups))
+        self.gmap_keys = sorted(self.gmap.keys())
+        return self
 
-    global gmap
-    gmap = dict(map(lambda x: (x.name, x), glist))
+    def from_store(self):
+        with open(self.store_file) as f:
+            lines = f.readlines()
+        lines = map(lambda x: x.strip(), lines)
+        lines = filter(lambda x: len(x) > 0, lines)
+        # lines = list(lines)
+        self.groups = []
+        for line in lines:
+            g = Group().loads(line)
+            self.groups.append(g)
+        self._init_internal()
+        return self
 
-    global gmap_keys
-    gmap_keys = sorted(gmap.keys())
+    def write_store(self):
+        with open(self.store_file, "w") as f:
+            for g in self.groups:
+                f.write(g.dumps())
+                f.write("\n")
+
+    def where_used(self, search_for):
+        return list(
+            filter(
+                lambda x: x.name == search_for or search_for in x.member, self.groups
+            )
+        )
+
+    def find(self, search_for):
+        return self.gmap[search_for]
 
 
 def dumps(l):
@@ -62,19 +106,18 @@ def dumps(l):
         print(grp.dumps())
 
 
-get_all_groups()
+repo = GroupRepo().read_current()
 
-dumps(glist)
-
+dumps(repo.groups)
 
 search_for = "sudo"
 
 print("--- search_for found", search_for)
 
-used = list(filter(lambda x: x.name == search_for or search_for in x.member, glist))
+used = repo.where_used(search_for)
 
 dumps(used)
 
 print("--- group", search_for)
 
-dumps([gmap[search_for]])
+dumps([repo.find(search_for)])
