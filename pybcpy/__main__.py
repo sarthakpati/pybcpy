c0b0d8a15d3b71c4f9a07eb5cd278a126b7aa8a9
kr-g <k.r.goger@gmail.com>
2021-10-16 06:02:47 +0200

    prepare new version

--- pybcpy/__main__.py
+++ pybcpy/__main__.py
@@ -9,7 +9,7 @@ from .file_utils import get_file_info, ensure_dest_dir
 from .diff_bak_copy import DiffBackup, DIFF_FILES, CFG_ALL_FILES_Default
 
 
-VERSION = "v0.0.16"
+VERSION = "v0.0.17-a"
 VERSION_add = "this software is in alpha state. refer homepage on github for more"
 
 BAK_NAME_ENV = "%BAK_NAME"
